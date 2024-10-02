0d8e005e6b200743bbac50b89524c87b5fdb38a5
KyleCoffie <160148467+KyleCoffie@users.noreply.github.com>
2024-10-02 15:10:05 -0500

    first commit

---
+++ Grocery store math.py
@@ -0,0 +1,9 @@
+#Task 1: Grocery Store Math Calculate the total cost of three items you'd commonly find in a grocery store, given their individual prices. 
+# For example, what would be the cost of bread, peanut butter, and jelly be? 
+# Prices don't need to be realistic!
+
+eggs = 5.99
+cheese = 8.99
+rice = 3.49
+total = eggs+cheese+rice
+print (total)
---
+++ showcase your dance moves task 2.py
@@ -0,0 +1,8 @@
+#Task 2: Your Mood Today Ask the user how they feel today. If they say "happy", print "That's great to hear!", 
+# and if they say "sad", print "I hope your day gets better!". Ensure your if statement is properly indented.
+mood = input ("How do you feel today? ")
+
+if mood == "Happy":
+    print ("Thats great to hear!")
+if mood == "Sad" :
+    print ("I hope your day gets better!")
